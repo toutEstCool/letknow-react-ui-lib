912b8149025911b108ba6217c78b38d7b5463af4
Danil Panarin <danilpanarin@thefrontends-MacBook-Air.local>
2023-11-01 17:51:04 +0600

    initial: initial ui-kit

---
+++ src/Button/Button.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import cls from './Button.module.scss'
+
+export const Button = () => <button className={cls.Button} disabled>Hello</button>
---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {Button} from "../Button/Button";
+
+
+const meta = {
+  title: 'Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
+
